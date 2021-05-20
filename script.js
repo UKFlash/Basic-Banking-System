b82b6b4545f38e5cb88e51d71e632bafee3e59f1
Uttam Kotiya <81281982+UKFlash@users.noreply.github.com>
2021-05-20 14:50:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+﻿let myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText);
+
+function sendMoney(){
+   var sender = document.getElementById("sender").value;
+   var receiver = document.getElementById("receiver").value;
+   var enterAmount = parseInt(document.getElementById("enterAmount").value);
+   
+   if (enterAmount < 500) {
+   	  alert("<500 amount not allowed")
+   } else if (enterAmount > 20000) {
+      alert("Insufficient Balance. Please enter Amount < 10000")
+   } else {
+      var findUserBankAccount = receiver + "BankBalance";
+      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
+      document.getElementById("myAccountBalance").innerText = myAccountBalance
+      document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+      alert(` Transaction Successful...!  
+      ${enterAmount}rs is sent to ${receiver}@gmail.com. 
+      Received by ${sender}@gmail.com
+      Thanks...!
+      Increment & Decrement money check below table..!`)
+
+      // transaction history 
+      var createPTag = document.createElement("ol");
+      var textNode = document.createTextNode(`${enterAmount}rs is sent to 
+      ${receiver}@gmail.com by 
+      ${sender}@gmail.com
+      ${Date()}`);
+      createPTag.appendChild(textNode);
+      var element = document.getElementById("transaction-history");
+      element.insertBefore(createPTag, element.firstChild);
+   }
+}
+
